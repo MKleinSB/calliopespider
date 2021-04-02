018b354f3799e1ca3c7760d2664c8c192c91d49a
Michael Klein <37311672+MKleinSB@users.noreply.github.com>
2021-04-02 17:31:24 +0200

    Vorwärts und rückwärts

--- main.ts
+++ main.ts
@@ -15,19 +15,6 @@ function BeinHeben (num: number) {
     Servo.Servonum(num + 4, Vorwaerts[num + 4])
     basic.pause(200)
 }
-input.onButtonPressed(Button.A, function () {
-    BeinHeben(0)
-    BeinVor(0)
-    BeinSenken(0)
-    BeinHeben(3)
-    BeinVor(3)
-    BeinSenken(3)
-})
-input.onButtonPressed(Button.AB, function () {
-    for (let Index3 = 0; Index3 <= 7; Index3++) {
-        Servo.Servonum(Index3, Stehwerte[Index3])
-    }
-})
 /**
  * Bein 1 Servo 0 und  Fuß 4
  * 
@@ -37,13 +24,29 @@ input.onButtonPressed(Button.AB, function () {
  * 
  * Bein 4 Servo 3 und Fuß 7
  */
+input.onButtonPressed(Button.A, function () {
+    for (let Index = 0; Index <= 3; Index++) {
+        BeinHeben(Index)
+        BeinZurück(Index)
+        BeinSenken(Index)
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    for (let Index3 = 0; Index3 <= 7; Index3++) {
+        Servo.Servonum(Index3, Stehwerte[Index3])
+    }
+})
 input.onButtonPressed(Button.B, function () {
-    BeinHeben(1)
-    BeinVor(1)
-    BeinSenken(1)
-    BeinHeben(2)
-    BeinVor(2)
-    BeinSenken(2)
+    for (let index = 0; index < 4; index++) {
+        for (let Index = 0; Index <= 3; Index++) {
+            BeinHeben(Index)
+            BeinVor(Index)
+            BeinSenken(Index)
+        }
+        for (let Index3 = 0; Index3 <= 7; Index3++) {
+            Servo.Servonum(Index3, Stehwerte[Index3])
+        }
+    }
 })
 // Bein 1 Servo 0 und 4
 // Bein 2 Servo 1 und 5
@@ -51,7 +54,7 @@ input.onButtonPressed(Button.B, function () {
 // Bein 4 Servo 3 und 7
 // Bein X Servo (x-1) und (x+3)
 function BeinZurück (num: number) {
-    Servo.Servonum(num, Startwerte[num])
+    Servo.Servonum(num, Rueckwaertswerte[num])
     basic.pause(200)
 }
 function BeinVor (num: number) {
@@ -60,7 +63,7 @@ function BeinVor (num: number) {
 }
 let Vorwaerts: number[] = []
 let Stehwerte: number[] = []
-let Startwerte: number[] = []
-Startwerte = [90, 98, 90, 90, 90, 90, 90, 90]
-Stehwerte = [120, 110, 120, 90, 50, 40, 50, 160]
-Vorwaerts = [170, 20, 170, 20, 90, 90, 60, 90]
+let Rueckwaertswerte: number[] = []
+Rueckwaertswerte = [60, 150, 70, 150, 90, 90, 90, 90]
+Stehwerte = [110, 110, 120, 90, 50, 40, 50, 160]
+Vorwaerts = [150, 70, 155, 40, 90, 90, 60, 90]

164b0e7964f3bc9d2bb84d0e8f1de9a6ecadf8bc
Michael Klein <37311672+MKleinSB@users.noreply.github.com>
2021-04-05 17:41:32 +0200

    Funk und Ultraschall hinzugefügt

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
+Ultraschall.onUltrasonicObjectDetected(20, DistanceUnit.CM, function () {
+    basic.setLedColor(0xffff00)
+})
+function Zurück () {
+    for (let Index = 0; Index <= 3; Index++) {
+        BeinHeben(Index)
+        BeinZurück(Index)
+        BeinSenken(Index)
+    }
+    for (let Index32 = 0; Index32 <= 7; Index32++) {
+        Servo.Servonum(Index32, Stehwerte[Index32])
+    }
+}
+Ultraschall.onUltrasonicObjectDetected(10, DistanceUnit.CM, function () {
+    basic.setLedColor(0xff0000)
+})
 // Bein 1 Servo 0 und 4
 // Bein 2 Servo 1 und 5
 // Bein 3 Servo 2 und 6
@@ -15,6 +31,20 @@ function BeinHeben (num: number) {
     Servo.Servonum(num + 4, Vorwaerts[num + 4])
     basic.pause(200)
 }
+input.onButtonPressed(Button.A, function () {
+    Zurück()
+    radio.sendString("R")
+})
+function Vor () {
+    for (let Index2 = 0; Index2 <= 3; Index2++) {
+        BeinHeben(Index2)
+        BeinVor(Index2)
+        BeinSenken(Index2)
+    }
+    for (let Index32 = 0; Index32 <= 7; Index32++) {
+        Servo.Servonum(Index32, Stehwerte[Index32])
+    }
+}
 /**
  * Bein 1 Servo 0 und  Fuß 4
  * 
@@ -24,29 +54,27 @@ function BeinHeben (num: number) {
  * 
  * Bein 4 Servo 3 und Fuß 7
  */
-input.onButtonPressed(Button.A, function () {
-    for (let Index = 0; Index <= 3; Index++) {
-        BeinHeben(Index)
-        BeinZurück(Index)
-        BeinSenken(Index)
-    }
-})
 input.onButtonPressed(Button.AB, function () {
     for (let Index3 = 0; Index3 <= 7; Index3++) {
         Servo.Servonum(Index3, Stehwerte[Index3])
     }
 })
-input.onButtonPressed(Button.B, function () {
-    for (let index = 0; index < 4; index++) {
-        for (let Index = 0; Index <= 3; Index++) {
-            BeinHeben(Index)
-            BeinVor(Index)
-            BeinSenken(Index)
-        }
-        for (let Index3 = 0; Index3 <= 7; Index3++) {
-            Servo.Servonum(Index3, Stehwerte[Index3])
-        }
+radio.onReceivedString(function (receivedString) {
+    Befehl = receivedString
+    if (Befehl == "V") {
+        Vor()
+    } else if (Befehl == "R") {
+        Zurück()
+    } else {
+    	
     }
+})
+Ultraschall.onUltrasonicObjectDetected(30, DistanceUnit.CM, function () {
+    basic.setLedColor(0x00ff00)
+})
+input.onButtonPressed(Button.B, function () {
+    Vor()
+    radio.sendString("V")
 })
 // Bein 1 Servo 0 und 4
 // Bein 2 Servo 1 und 5
@@ -64,6 +92,10 @@ function BeinVor (num: number) {
 let Vorwaerts: number[] = []
 let Stehwerte: number[] = []
 let Rueckwaertswerte: number[] = []
+let Befehl = ""
+Ultraschall.connectUltrasonicDistanceSensor(DigitalPin.C16, DigitalPin.C17)
+radio.setGroup(1)
+Befehl = "X"
 Rueckwaertswerte = [60, 150, 70, 150, 90, 90, 90, 90]
 Stehwerte = [110, 110, 120, 90, 50, 40, 50, 160]
 Vorwaerts = [150, 70, 155, 40, 90, 90, 60, 90]
---
+++ ultrasonic.ts
@@ -0,0 +1,221 @@
+const enum DistanceUnit {
+  //% block="cm"
+  CM = 58, // Duration of echo round-trip in Microseconds (uS) for two centimeters, 343 m/s at sea level and 20°C
+  //% block="inch"
+  INCH = 148, // Duration of echo round-trip in Microseconds (uS) for two inches, 343 m/s at sea level and 20°C
+}
+//% weight=100 color=#0fbc11 icon="\uf0A1"
+namespace Ultraschall {
+  const MICROBIT_MAKERBIT_ULTRASONIC_OBJECT_DETECTED_ID = 798;
+  const MAX_ULTRASONIC_TRAVEL_TIME = 300 * DistanceUnit.CM;
+  const ULTRASONIC_MEASUREMENTS = 3;
+
+  interface UltrasonicRoundTrip {
+    ts: number;
+    rtt: number;
+  }
+
+  interface UltrasonicDevice {
+    trig: DigitalPin | undefined;
+    roundTrips: UltrasonicRoundTrip[];
+    medianRoundTrip: number;
+    travelTimeObservers: number[];
+  }
+
+  let ultrasonicState: UltrasonicDevice;
+
+  /**
+   * Configures the ultrasonic distance sensor and measures continuously in the background.
+   * @param trig pin connected to trig, eg: DigitalPin.C16
+   * @param echo pin connected to echo, eg: DigitalPin.C17
+   */
+  //% blockId="makerbit_ultrasonic_connect"
+  //% block="Ultraschallsensor | mit Trig an %trig | und Echo an %echo"
+  //% trig.fieldEditor="gridpicker"
+  //% trig.fieldOptions.columns=4
+  //% trig.fieldOptions.tooltips="false"
+  //% echo.fieldEditor="gridpicker"
+  //% echo.fieldOptions.columns=4
+  //% echo.fieldOptions.tooltips="false"
+  //% weight=80
+  export function connectUltrasonicDistanceSensor(
+    trig: DigitalPin,
+    echo: DigitalPin
+  ): void {
+    if (ultrasonicState && ultrasonicState.trig) {
+      return;
+    }
+
+    if (!ultrasonicState) {
+      ultrasonicState = {
+        trig: trig,
+        roundTrips: [{ ts: 0, rtt: MAX_ULTRASONIC_TRAVEL_TIME }],
+        medianRoundTrip: MAX_ULTRASONIC_TRAVEL_TIME,
+        travelTimeObservers: [],
+      };
+    } else {
+      ultrasonicState.trig = trig;
+    }
+
+    pins.onPulsed(echo, PulseValue.High, () => {
+      if (
+        pins.pulseDuration() < MAX_ULTRASONIC_TRAVEL_TIME &&
+        ultrasonicState.roundTrips.length <= ULTRASONIC_MEASUREMENTS
+      ) {
+        ultrasonicState.roundTrips.push({
+          ts: input.runningTime(),
+          rtt: pins.pulseDuration(),
+        });
+      }
+    });
+
+    control.inBackground(measureInBackground);
+  }
+
+  /**
+   * Do something when an object is detected the first time within a specified range.
+   * @param distance distance to object, eg: 20
+   * @param unit unit of distance, eg: DistanceUnit.CM
+   * @param handler body code to run when the event is raised
+   */
+  //% blockId=makerbit_ultrasonic_on_object_detected
+  //% block="Objekt erkannt in | %distance | %unit"
+  //% weight=69
+  export function onUltrasonicObjectDetected(
+    distance: number,
+    unit: DistanceUnit,
+    handler: () => void
+  ) {
+    if (distance <= 0) {
+      return;
+    }
+
+    if (!ultrasonicState) {
+      ultrasonicState = {
+        trig: undefined,
+        roundTrips: [{ ts: 0, rtt: MAX_ULTRASONIC_TRAVEL_TIME }],
+        medianRoundTrip: MAX_ULTRASONIC_TRAVEL_TIME,
+        travelTimeObservers: [],
+      };
+    }
+
+    const travelTimeThreshold = Math.imul(distance, unit);
+
+    ultrasonicState.travelTimeObservers.push(travelTimeThreshold);
+
+    control.onEvent(
+      MICROBIT_MAKERBIT_ULTRASONIC_OBJECT_DETECTED_ID,
+      travelTimeThreshold,
+      () => {
+        handler();
+      }
+    );
+  }
+
+  /**
+   * Returns the distance to an object in a range from 1 to 300 centimeters or up to 118 inch.
+   * The maximum value is returned to indicate when no object was detected.
+   * -1 is returned when the device is not connected.
+   * @param unit unit of distance, eg: DistanceUnit.CM
+   */
+  //% blockId="makerbit_ultrasonic_distance"
+  //% block="Entfernung %unit"
+  //% weight=60
+  export function getUltrasonicDistance(unit: DistanceUnit): number {
+    if (!ultrasonicState) {
+      return -1;
+    }
+    basic.pause(0); // yield to allow background processing when called in a tight loop
+    return Math.idiv(ultrasonicState.medianRoundTrip, unit);
+  }
+
+  /**
+   * Returns `true` if an object is within the specified distance. `false` otherwise.
+   *
+   * @param distance distance to object, eg: 20
+   * @param unit unit of distance, eg: DistanceUnit.CM
+   */
+  //% blockId="makerbit_ultrasonic_less_than"
+  //% block="Entfernung geringer als | %distance | %unit"
+  //% weight=50
+  export function isUltrasonicDistanceLessThan(
+    distance: number,
+    unit: DistanceUnit
+  ): boolean {
+    if (!ultrasonicState) {
+      return false;
+    }
+    basic.pause(0); // yield to allow background processing when called in a tight loop
+    return Math.idiv(ultrasonicState.medianRoundTrip, unit) < distance;
+  }
+
+  function triggerPulse() {
+    // Reset trigger pin
+    pins.setPull(ultrasonicState.trig, PinPullMode.PullNone);
+    pins.digitalWritePin(ultrasonicState.trig, 0);
+    control.waitMicros(2);
+
+    // Trigger pulse
+    pins.digitalWritePin(ultrasonicState.trig, 1);
+    control.waitMicros(10);
+    pins.digitalWritePin(ultrasonicState.trig, 0);
+  }
+
+  function getMedianRRT(roundTrips: UltrasonicRoundTrip[]) {
+    const roundTripTimes = roundTrips.map((urt) => urt.rtt);
+    return median(roundTripTimes);
+  }
+
+  // Returns median value of non-empty input
+  function median(values: number[]) {
+    values.sort((a, b) => {
+      return a - b;
+    });
+    return values[(values.length - 1) >> 1];
+  }
+
+  function measureInBackground() {
+    const trips = ultrasonicState.roundTrips;
+    const TIME_BETWEEN_PULSE_MS = 145;
+
+    while (true) {
+      const now = input.runningTime();
+
+      if (trips[trips.length - 1].ts < now - TIME_BETWEEN_PULSE_MS - 10) {
+        ultrasonicState.roundTrips.push({
+          ts: now,
+          rtt: MAX_ULTRASONIC_TRAVEL_TIME,
+        });
+      }
+
+      while (trips.length > ULTRASONIC_MEASUREMENTS) {
+        trips.shift();
+      }
+
+      ultrasonicState.medianRoundTrip = getMedianRRT(
+        ultrasonicState.roundTrips
+      );
+
+      for (let i = 0; i < ultrasonicState.travelTimeObservers.length; i++) {
+        const threshold = ultrasonicState.travelTimeObservers[i];
+        if (threshold > 0 && ultrasonicState.medianRoundTrip <= threshold) {
+          control.raiseEvent(
+            MICROBIT_MAKERBIT_ULTRASONIC_OBJECT_DETECTED_ID,
+            threshold
+          );
+          // use negative sign to indicate that we notified the event
+          ultrasonicState.travelTimeObservers[i] = -threshold;
+        } else if (
+          threshold < 0 &&
+          ultrasonicState.medianRoundTrip > -threshold
+        ) {
+          // object is outside the detection threshold -> re-activate observer
+          ultrasonicState.travelTimeObservers[i] = -threshold;
+        }
+      }
+
+      triggerPulse();
+      basic.pause(TIME_BETWEEN_PULSE_MS);
+    }
+  }
+}
